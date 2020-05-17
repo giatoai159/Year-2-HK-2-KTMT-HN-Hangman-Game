20f2e73eb83ad098eafb47e580975a4e9669d18e
Huỳnh Gia Toại <giatoai159@gmail.com>
2020-05-18 00:21:25 +0700

    Update Hangman.asm

--- Hangman.asm
+++ Hangman.asm
@@ -1,21 +1,35 @@
 .data
 	SoChanRandom: .word 0
 	MaxRandom: .word 0
+	ChuCaiDaDoan: .word 0
+	tempString: .word 0
 .text
 	.globl main
 main:
-	li $v0,5
+
+	#Test ham random so chan
+#	li $v0,5
+#	syscall
+#	sw $v0,MaxRandom
+#	lw $a1,MaxRandom
+#	jal _randomSoChan
+#	sw $v0,SoChanRandom
+#	li $v0,1
+#	lw $a0,SoChanRandom
+#	syscall
+	#Test ham random so chan - End
+	li $v0,8
+	la $a0,ChuCaiDaDoan
+	la $a1,26
 	syscall
-	sw $v0,MaxRandom
-	lw $a1,MaxRandom
-	jal _randomSoChan
-	sw $v0,SoChanRandom
-	li $v0,1
-	lw $a0,SoChanRandom
-	syscall
+	lw $a0,ChuCaiDaDoan
+	li $a1,65
+	li $a2,77
+	jal _InChuCai
 	# ket thuc chuong trinh
 	li $v0,10
 	syscall
+#----------------------------------------------------------------------------------------------------------------------------------------#
 _randomSoChan:
 	# Dau thu tuc
 	addi $sp,$sp,-32
@@ -51,3 +65,17 @@ _randomSoChan.thoatMax:
 	addi $sp,$sp,32
 	#Quay ve
 	jr $ra
+
+#----------------------------------------------------------------------------------------------------------------------------------------# WIP
+_InChuCai:
+	#Dau thu tuc
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+
+	#Than thu tuc
+_InChuCai.Loop1:
+	bgt $a1,$a2,_InChuCai.ExitLoop1
+	
+	addi $a1,$a1,1
+	j _InChuCai.Loop1
+_InChuCai.ExitLoop1:
