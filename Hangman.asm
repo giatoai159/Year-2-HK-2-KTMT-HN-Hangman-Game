8d3d0bb5d56cc3486940712a02172320dbc06b7d
Huỳnh Gia Toại <giatoai159@gmail.com>
2020-05-18 10:18:07 +0700

    Update Hangman.asm

--- Hangman.asm
+++ Hangman.asm
@@ -3,6 +3,7 @@
 	MaxRandom: .word 0
 	ChuCaiDaDoan: .word 0
 	tempString: .word 0
+	test: .asciiz "   "
 .text
 	.globl main
 main:
@@ -30,6 +31,14 @@ main:
 	li $v0,10
 	syscall
 #----------------------------------------------------------------------------------------------------------------------------------------#
+#int randomSoChan( int max)
+#{
+#	if (max % 2 == 1)
+#		max--;
+#	int random = (rand() % (max + 1) / 2;
+#	return random * 2;
+#}
+# Ham random so chan INPUT: MAX la $a1, OUTPUT: so ngau nhien tu 0~MAX tai $v0
 _randomSoChan:
 	# Dau thu tuc
 	addi $sp,$sp,-32
@@ -37,6 +46,7 @@ _randomSoChan:
 	sw $s0,4($sp)
 	sw $t0,8($sp)
 	sw $t1,12($sp)
+	sw $a1,16($sp)
 	# Than thu tuc
 	li $t0,2
 	div $a1,$t0 # MAX % 2
@@ -61,6 +71,7 @@ _randomSoChan.thoatMax:
 	lw $s0,4($sp)
 	lw $t0,8($sp)
 	lw $t1,12($sp)
+	lw $a1,16($sp)
 	# Xoa Stack
 	addi $sp,$sp,32
 	#Quay ve
@@ -73,9 +84,42 @@ _InChuCai:
 	sw $ra,($sp)
 
 	#Than thu tuc
-_InChuCai.Loop1:
-	bgt $a1,$a2,_InChuCai.ExitLoop1
+	# Tinh chieu dai cua string
+	la $t0,ChuCaiDaDoan
+	li $t1,0
+_InChuCai.strLength:
+	lb $t2,($t0)
+	bne $t2,'\n',_InChuCai.strLengthTangDem
+	j _InChuCai.strLengthEnd
+_InChuCai.strLengthTangDem:
+	addi $t1,$t1,1
+	addi $t0,$t0,1
+	j _InChuCai.strLength
+_InChuCai.strLengthEnd:
+	addi $t0,$t0,-1
+	# output t1 la strLength
+	la $t0,ChuCaiDaDoan # restore t0
 	
+_InChuCai.Loop1:
+	bgt $a1,$a2,_InChuCai.ExitLoop1 # Chay tu from -> to tu du lieu truyen vao
+
+#### WIP - Eu biet lam
+	li $t2,0 # dem = 0
+_InChuCai.Loop2:
+	bge $t2,$t1,_InChuCai.Loop2.Exit
+	lb $t3,($t0)
+	bne $t3,$a1,_InChuCai.Loop3
+	
+_InChuCai.Loop2.Cont:
+	addi $t2,$t2,1
+	addi $t0,$t0,1
+	j _InChuCai.Loop2
+_InChuCai.Loop3:
+	
+	j _InChuCai.Loop2.Cont
+_InChuCai.Loop2.Exit:
+	
+#### WIP
 	addi $a1,$a1,1
 	j _InChuCai.Loop1
 _InChuCai.ExitLoop1:
