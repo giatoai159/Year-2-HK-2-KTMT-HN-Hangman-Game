d1037fa5015392c2b4dd5989400777d7450fd3ef
nguyentanvinh7a <43849741+nguyentanvinh7a@users.noreply.github.com>
2020-05-20 23:06:38 +0700

    Thêm hàm InChu, InChuCai
    Các hàm con: 
    string.suppend(): cộng 1 ký tự vào chuỗi ban đầu
     string.length(): trả về độ dài chuỗi
    string.find(): tìm kiếm trong chuỗi có ký tự được truyền vào, trả về vị trí đầu tiên xuất hiện, không tìm thấy trả về -1

--- Hangman.asm
+++ Hangman.asm
@@ -16,6 +16,8 @@
 	InHangMan4Dong4: .asciiz "|       /|\n"
 	InHangMan5Dong4: .asciiz "|       /|\\\n"
 	InHangMan7Dong5: .asciiz "|       / \\\n"
+	vien: .asciiz "\n+------------------------------------------------+\n"
+	res: .asciiz ""
 #--------------------------------------------------------------------VINH---------------------------------------------------------------------#
 #--------------------------------------------------------------------TUAN---------------------------------------------------------------------#
 
@@ -320,6 +322,299 @@ InHangMan.7:
 	li $v0,4
 	syscall
 	j _InHangMang.KetThuc
+#----------------------------------------------------------------------
+_InChuCai: #void InChuCai(string chucaidoan, char tu, char den)
+#$a0: chucaidoan, $s1: tu, $s2: den
+#Dau thu tuc:
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+	sw $s7,4($sp)
+	sw $s0,8($sp)
+	sw $s1,12($sp)
+	sw $s2,16($sp)
+	sw $t0,20($sp)
+	sw $t2,24($sp)
+	sw $t1,28($sp)
+#Khoi tao
+	move $s0,$a0 #string ban dau
+	move $s1,$a1
+	move $s2,$a2
+	move $t0,$a1 #bien i
+	la $s7,res #bien luu tru ket qua de in ra
+#Than thu tuc
+_InChuCai.Lap:
+	move $a0,$t0
+	la $a1,($s0)
+	jal _Tim
+	move $t2,$v0
+
+	beq $t2,-1,InChuCai.CongKyTu1
+	InChuCai.CongKyTu.TiepTuc1:
+
+	beq $t2,-1,InChuCai.CongKyTu2
+	InChuCai.CongKyTu.TiepTuc2:
+
+	beq $t2,-1,InChuCai.CongKyTu5
+	InChuCai.CongKyTu.TiepTuc5:
+
+	bne $t2,-1,InChuCai.CongKyTu3
+	InChuCai.CongKyTu.TiepTuc3:
+
+	bne $t2,-1,InChuCai.CongKyTu4
+	InChuCai.CongKyTu.TiepTuc4:
+
+	bne $t2,-1,InChuCai.CongKyTu6
+	InChuCai.CongKyTu.TiepTuc6:
+
+	addi $t0,$t0,1
+	addi $s0,$s0,1
+	sub $t1,$s2,$t0 #$t1=$s2-$t0
+	bgez $t1,_InChuCai.Lap #Neu $t1<=0 thi lap
+
+	#Truyen tham so cho ham _InChu
+	la $a0,($s7)
+	li $a1,0
+	li $a2,0
+	jal _InChu
+#Cuoi thu tuc
+	lw $ra,($sp)
+	lw $s7,4($sp)
+	lw $s0,8($sp)
+	lw $s1,12($sp)
+	lw $s2,16($sp)
+	lw $t0,20($sp)
+	lw $t2,24($sp)
+	lw $t1,28($sp)
+	addi $sp,$sp,32
+	jr $ra
+#Ket thuc
+	j _KetThuc
+	
+_Tim: #Ham nhan vao 1 chuoi va 1 ky tu, tra ve vi tri dau tien cua ky tu trong chuoi do duoc tim thay, neu khong tim thay return -1
+#$a0: ky tu, $a1: chuoi, $v0: vi tri
+#Dau thu tuc
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+	sw $t0,4($sp)
+	sw $t1,8($sp)
+	
+#Khai bao
+	li $v0,-1 #Ket qua tra ve
+	li $t0,0 #Bien lap i
+#Than thu tuc
+_Tim.Lap:
+	lb $t1,($a1)
+	#Tang dem
+	addi $t0,$t0,1
+	addi $a1,$a1,1
+	beq $t1,$a0,_Tim.Lap.TraVe
+	bne $t1,'\0',_Tim.Lap
+	_Tim.Lap.TraVe.TiepTuc:
+#Cuoi thu tuc
+	lw $ra,($sp)
+	lw $t0,4($sp)
+	lw $t1,8($sp)
+	addi $sp,$sp,32
+#Ket thuc
+	jr $ra
+_Tim.Lap.TraVe:
+	move $v0,$t0
+	addi $v0,$v0,-1
+	j _Tim.Lap.TraVe.TiepTuc
+
+InChuCai.CongKyTu1:
+	move $a0,$t0
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc1
+InChuCai.CongKyTu2:
+	li $a0,' '
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc2
+InChuCai.CongKyTu5:
+	li $a0,' '
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc5
+InChuCai.CongKyTu3:
+	li $a0,' '
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc3
+InChuCai.CongKyTu4:
+	li $a0,' '
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc4
+InChuCai.CongKyTu6:
+	li $a0,' '
+	move $a1,$s7
+	jal _CongKyTu
+	move $s7,$v0
+	j InChuCai.CongKyTu.TiepTuc6
+_CongKyTu: #$a0: ky tu can cong, $a1: chuoi ban dau truyen vao
+#Dau thu tuc
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+	sw $t0,4($sp)
+	sw $t1,8($sp)
+	sw $s0,12($sp)
+#Khai bao
+	li $t1,0 #bien i
+	la $s0,($a1)#s0 la bien luu ket qua
+#Than thu tuc
+_CongKyTu.Lap: #Moi vong lap gan str[i] vao $v0[i]
+	lb $t0,($a1)
+	sb $t0,($s0)
+	addi $a1,$a1,1
+	addi $s0,$s0,1
+	addi $t1,$t1,1
+	bne $t0,'\0',_CongKyTu.Lap
+	#Gan $v0[str.lenght()]=$a0
+	addi $s0,$s0,-1
+	sb $a0,($s0)
+	#Gan phan tu ket thuc chuoi \0 vao cuoi chuoi $v0
+	addi $s0,$s0,1
+	li $a0,'\0'
+	sb $a0,($s0)
+	#Tra ve dia chi ban dau cho $s0
+	sub $s0,$s0,$t1
+	move $v0,$s0
+#Cuoi thu tuc
+	lw $ra,($sp)
+	lw $t0,4($sp)
+	lw $t1,8($sp)
+	lw $s0,12($sp)
+	addi $sp,$sp,32
+	jr $ra
+#----------------------------------------------------------------------
+_InChu: #Ham in ra 1 chuoi dang |          aaaa         |
+#$a0: chuoi can in, $a1(bool): in vien tren, $a2(bool): in vien duoi
+#Dau thu tuc
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+	sw $t0,4($sp)
+	sw $t1,8($sp)
+	sw $t2,12($sp)
+	sw $t3,16($sp)
+	sw $t4,20($sp)
+	sw $a3,24($sp)
+#Than thu tuc
+	move $a3,$a0
+	la $a0,vien
+	beq $a1,1,_InChu.InVien1
+	_InChu.InVien1.TiepTuc:
+	#In dau |
+	la $a0,'|'
+	li $v0,11
+	syscall
+
+	#Xac dinh khoang trong co do lon bao nhieu
+	move $a0,$a3
+	jal _StringLength #Xac dinh do dai cua input
+	
+	li $t0,2
+	div $v0,$t0
+	mflo $t1 #t3 la do dai cua khoang trong
+	mfhi $t4 #t4 la so du khi chia do dai input cho 2
+	li $t2,24
+	sub $t3,$t2,$t1
+	blez $t3,_InChu.VuotGioiHan
+	li $t0,0
+_InChu.InKhoangTrong1:
+	la $a0,' '
+	li $v0,11
+	syscall
+	addi $t0,$t0,1
+	bne $t0,$t3,_InChu.InKhoangTrong1
+
+	#In chuoi
+	move $a0,$a3
+	li $v0,4
+	syscall
+
+	li $t0,0
+	addi $t3,$t3,-1
+_InChu.InKhoangTrong2:
+	la $a0,' '
+	li $v0,11
+	syscall
+	addi $t0,$t0,1
+	bne $t0,$t3,_InChu.InKhoangTrong2
+
+	beq $t4,1,_InChu.Nhay
+	beqz $t4,_InChu.InDauCach
+	_InChu.InDauCach:
+	la $a0,' '
+	li $v0,11
+	syscall
+	_InChu.Nhay:
+	_InChu.VuotGioiHan.TiepTuc:
+	#In dau |
+	la $a0,'|'
+	li $v0,11
+	syscall
+	
+	la $a0,vien
+	beq $a2,1,_InChu.InVien2
+	_InChu.InVien2.TiepTuc:
+#Cuoi thu tuc:
+	lw $ra,($sp)
+	lw $ra,($sp)
+	lw $t0,4($sp)
+	lw $t1,8($sp)
+	lw $t2,12($sp)
+	lw $t3,16($sp)
+	lw $t4,20($sp)
+	lw $a3,24($sp)
+	addi $sp,$sp,32
+	jr $ra
+#Ket thuc
+	j _KetThuc
+_InChu.InVien1:
+	li $v0,4
+	syscall
+	j _InChu.InVien1.TiepTuc
+_InChu.InVien2:
+	li $v0,4
+	syscall
+	j _InChu.InVien2.TiepTuc
+_InChu.VuotGioiHan:
+	#In chuoi
+	move $a0,$a3
+	li $v0,4
+	syscall
+	j _InChu.VuotGioiHan.TiepTuc
+_StringLength: #Ham tra ve do dai 1 string, $a0: chuoi, $v0: do dai chuoi
+#Dau thu tuc
+	addi $sp,$sp,-32
+	sw $ra,($sp)
+	sw $t0,4($sp)
+#Khoi tao
+	li $v0,0
+#Than thu tuc
+_StringLength.Loop:
+	lb $t0,($a0)
+	addi $a0,$a0,1
+	addi $v0,$v0,1
+	bne $t0,'\0',_StringLength.Loop
+#Cuoi thu tuc
+	addi $v0,$v0,-1
+	lw $ra,($sp)
+	lw $t0,4($sp)
+	addi $sp,$sp,32
+	jr $ra
+
+	_KetThuc:
+	li $v0,10
+	syscall
+
 #--------------------------------------------------------------------VINH---------------------------------------------------------------------#
 
 #--------------------------------------------------------------------TUAN---------------------------------------------------------------------#
